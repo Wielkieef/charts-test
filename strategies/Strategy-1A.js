5495bc1eeeef744ce41b77924638ed70d5e56f47
Wielkieef <filip.koryzna.1997@gmail.com>
2025-03-24 17:45:50 +0100

    Update Strategy-1A.js

--- strategies/Strategy-1A.js
+++ strategies/Strategy-1A.js
@@ -1,7 +1,26 @@
+// strategies/Strategy-1A.js
 export function getStrategy1AData() {
   return [
-    { time: '2023-03-01', value: 100 },
-    { time: '2023-03-02', value: 105 },
-    { time: '2023-03-03', value: 102 },
+    {
+      time: '2023-03-01',
+      open: 100,
+      high: 106,
+      low: 99,
+      close: 105,
+    },
+    {
+      time: '2023-03-02',
+      open: 105,
+      high: 107,
+      low: 102,
+      close: 103,
+    },
+    {
+      time: '2023-03-03',
+      open: 103,
+      high: 104,
+      low: 100,
+      close: 102,
+    },
   ];
 }
