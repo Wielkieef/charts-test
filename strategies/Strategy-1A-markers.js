4eb21d005080919f11a52f0f763b72df1b5ef07c
Wielkieef <filip.koryzna.1997@gmail.com>
2025-03-24 17:32:50 +0100

    Update Strategy-1A-markers.js

--- strategies/Strategy-1A-markers.js
+++ strategies/Strategy-1A-markers.js
@@ -1,1 +1,20 @@
 
+// Strategy-1A-markers.js
+export function getStrategy1AMarkers() {
+  return [
+    {
+      time: '2023-03-01',
+      position: 'aboveBar',
+      color: 'blue',
+      shape: 'arrowUp',
+      text: 'Long'
+    },
+    {
+      time: '2023-03-03',
+      position: 'belowBar',
+      color: 'purple',
+      shape: 'arrowDown',
+      text: 'Close'
+    }
+  ];
+}
